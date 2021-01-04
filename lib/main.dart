3c29f69d6af7ac2b53eb61b77cc7639ba1aae7a2
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2021-01-04 17:24:01 +0600

    Safearea widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home Page'),
-          centerTitle: true,
-          leading: Icon(Icons.message),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.add_a_photo), onPressed: (){})
-          ],
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Home Page'),
+      //     centerTitle: true,
+      //     leading: Icon(Icons.message),
+      //     actions: <Widget>[
+      //       IconButton(icon: Icon(Icons.add_a_photo), onPressed: (){})
+      //     ],
+      //   ),
+      //     body: Center(
+      //       child: Text("This is my first app", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.redAccent),),
+      //     )
+      // ),
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.red,
         ),
-          body: Center(
-            child: Text("This is my first app", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.redAccent),),
-          )
       ),
 
     );
